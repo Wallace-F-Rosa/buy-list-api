476eb8bb53d466b595f739a55986f5ec74859be7
Wallace Ferancini Rosa <wferancini@gmail.com>
2024-04-01 23:13:57 -0300

    ingredient: added update test

--- server/router_test.go
+++ server/router_test.go
@@ -7,44 +7,53 @@ import (
 	"meal-planner/planner/database"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
-func setup() (*gin.Engine, *httptest.ResponseRecorder) {
+func setup() (*gin.Engine, *httptest.ResponseRecorder, *gorm.DB) {
 	db := database.GetDatabaseConnection()
 	router := GetRouter(db)
 	recorder := httptest.NewRecorder()
-	return router, recorder
-}
-
-func TestPingRoute(t *testing.T) {
-	router, w := setup()
-
-	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	var result map[string]any
-	json.Unmarshal(w.Body.Bytes(), &result)
-	assert.Equal(t, "pong", result["message"])
+	return router, recorder, db
 }
 
 func TestIngredientCreate(t *testing.T) {
-	router, recorder := setup()
+	router, recorder, _ := setup()
 
 	ingredient := &planner.Ingredient{Name: "bread", OriginType: "plant"}
 	ingredientJson, _ := json.Marshal(ingredient)
 	body := bytes.NewBuffer(ingredientJson)
 
-	req, _ := http.NewRequest("POST", "/ingredient", body)
+	req, _ := http.NewRequest("POST", "/api/ingredient", body)
 	router.ServeHTTP(recorder, req)
 
 	assert.Equal(t, http.StatusCreated, recorder.Code)
 	var result planner.Ingredient
 	json.Unmarshal(recorder.Body.Bytes(), &result)
+	assert.Equal(t, ingredient.Name, result.Name)
+	assert.Equal(t, ingredient.OriginType, result.OriginType)
+}
+
+func TestIngredientUpdate(t *testing.T) {
+	router, recorder, db := setup()
+
+	service := &planner.IngredientService{Database: db}
+	ingredient, _ := service.Create("test", "testing")
+	ingredientJson, _ := json.Marshal(ingredient)
+	jsonBody := bytes.NewBuffer(ingredientJson)
+
+	req, _ := http.NewRequest("PUT", "/ingredient/"+strconv.FormatUint(uint64(ingredient.ID), 10), jsonBody)
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	var result planner.Ingredient
+	json.Unmarshal(recorder.Body.Bytes(), &result)
+
 	assert.Equal(t, ingredient.ID, result.ID)
 	assert.Equal(t, ingredient.Name, result.Name)
 	assert.Equal(t, ingredient.OriginType, result.OriginType)
