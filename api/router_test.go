7cb3998195d913c475879aec131e04c33f8836a3
Wallace Ferancini Rosa <wferancini@gmail.com>
2024-06-20 07:04:04 -0300

    integration tests: fix recorder not being initialized correctly

--- api/router_test.go
+++ api/router_test.go
@@ -6,17 +6,28 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
+func LoadEnv() {
+	// load .env file
+	err := godotenv.Load("..\\.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func setup() (*gin.Engine, *httptest.ResponseRecorder, *gorm.DB) {
+	LoadEnv()
 	db := database.GetDatabaseConnection()
 	router := GetRouter(db)
 	recorder := httptest.NewRecorder()
@@ -26,6 +37,7 @@ func setup() (*gin.Engine, *httptest.ResponseRecorder, *gorm.DB) {
 var router, recorder, db = setup()
 
 func TestIngredientCreate(t *testing.T) {
+	recorder := httptest.NewRecorder()
 	ingredient := &internal.Ingredient{Name: "bread", OriginType: "plant"}
 	ingredientJson, _ := json.Marshal(ingredient)
 	body := bytes.NewBuffer(ingredientJson)
@@ -41,6 +53,7 @@ func TestIngredientCreate(t *testing.T) {
 }
 
 func TestIngredientUpdate(t *testing.T) {
+	recorder := httptest.NewRecorder()
 	service := &internal.IngredientService{Database: db}
 	ingredient, _ := service.Create("test", "testing")
 	ingredientJson, _ := json.Marshal(ingredient)
@@ -59,6 +72,7 @@ func TestIngredientUpdate(t *testing.T) {
 }
 
 func TestIngredientDelete(t *testing.T) {
+	recorder := httptest.NewRecorder()
 	service := &internal.IngredientService{Database: db}
 	ingredient, _ := service.Create("test delete", "testing")
 
@@ -75,6 +89,7 @@ func TestIngredientDelete(t *testing.T) {
 }
 
 func TestIngredientFind(t *testing.T) {
+	recorder := httptest.NewRecorder()
 	service := &internal.IngredientService{Database: db}
 	ingredient, _ := service.Create("test find", "testing")
 
@@ -102,22 +117,32 @@ func TestIngredientFindByParams(t *testing.T) {
 	service := &internal.IngredientService{Database: db}
 	ingredient, _ := service.Create("test find", "testing")
 
-	req, _ := http.NewRequest("GET", fmt.Sprintf("/api/ingredient?name=%s", "find"), nil)
-	router.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusOK, recorder.Code)
-
-	var result []internal.Ingredient
-	json.Unmarshal(recorder.Body.Bytes(), &result)
-	assert.NotEmpty(t, result)
-	resultMap := make(map[uint]internal.Ingredient, len(result))
-	for _, ingredient := range result {
-		resultMap[ingredient.ID] = ingredient
+	query := []string{
+		"name=find",
+		"originType=testing",
+		"name=find&originType=testing",
 	}
 
-	value, exists := resultMap[ingredient.ID]
-	assert.True(t, exists)
-	assert.Equal(t, ingredient.ID, value.ID)
-	assert.Equal(t, ingredient.Name, value.Name)
-	assert.Equal(t, ingredient.OriginType, value.OriginType)
+	for _, param := range query {
+		recorder := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", fmt.Sprintf("/api/ingredient?%s", param), nil)
+		router.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusOK, recorder.Code)
+
+		var result []internal.Ingredient
+		json.Unmarshal(recorder.Body.Bytes(), &result)
+		assert.NotEmpty(t, result)
+		resultMap := make(map[uint]internal.Ingredient, len(result))
+		for _, ingredient := range result {
+			resultMap[ingredient.ID] = ingredient
+		}
+
+		value, exists := resultMap[ingredient.ID]
+		assert.True(t, exists)
+		assert.Equal(t, ingredient.ID, value.ID)
+		assert.Equal(t, ingredient.Name, value.Name)
+		assert.Equal(t, ingredient.OriginType, value.OriginType)
+	}
+
 }
