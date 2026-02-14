7f810c6703f17efaa3e76d614be7fdb49908279d
Wallace Ferancini Rosa <wferancini@gmail.com>
2026-02-14 14:54:26 -0300

    Init spring boot project

---
+++ src/test/java/com/project/buylist/BuylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.buylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
